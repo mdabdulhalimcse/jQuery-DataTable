86949685eff28365f4dc1bfe3d114a107ab8cfcc
Md. Abdul Halim <mdabdulhalimcse@gmail.com>
2022-11-28 01:02:08 +0600

    jQuery DataTable initiation

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Datatable from "datatable";
+import SimpleDataTable from "./jquery-datatable/SimpleDataTable";
+import SimpleJquaryDataTable from "./jquery-datatable/SimpleJquaryDataTable";
+import App1 from "./jquery/App1";
+import DataTable from "./jquery/DataTable";
+
+
+function App() {
+  return (
+    <div >
+      {/* <SimpleJquaryDataTable />
+      <App1 /> */}
+
+      {/* <SimpleDataTable /> */}
+
+      <DataTable />
+      
+
+    </div>
+  );
+}
+
+export default App;
